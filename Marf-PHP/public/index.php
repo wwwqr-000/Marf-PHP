3afb8255344c0af31a5b2e322b7326252a06e69d
wwwqr-000 <wwwqrminecraft@gmail.com>
2025-02-18 20:10:57 +0100

    added comment

--- Marf-PHP/public/index.php
+++ Marf-PHP/public/index.php
@@ -3,6 +3,6 @@ session_start();
 require_once("../client.php");
 require_once("../routes.php");
 
-Routes::setIgnore("/Marf-PHP/Marf-PHP/public");
+Routes::setIgnore("/Marf-PHP/Marf-PHP/public");//Path here get's removed from route: "http://localhost/Marf-PHP/Marf-PHP/public/" -> "/"
 Routes::redir();
 ?>

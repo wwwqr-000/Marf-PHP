e0ad86dda1a2f2660f6879f2d3efaa414ee3f61c
wwwqr-000 <wwwqrminecraft@gmail.com>
2025-03-02 14:38:38 +0100

    Added hash method to Security

--- Marf-PHP/security.php
+++ Marf-PHP/security.php
@@ -4,20 +4,26 @@ class Security {
     private static $aesKey;
     private static $algoName;
     private static $algoType;
+    private static $algoHash;
 
     public static function init() {
         self::$ivKey = "1234567890123456";//16 chars (This is an example iv-key! Replace!)
         self::$aesKey = "12345678901234567890123456789012";//32 chars (This is an example aes-key! Replace!)
         self::$algoName = "aes-256-cbc";
         self::$algoType = OPENSSL_RAW_DATA;
+        self::$algoHash = "sha256";
     }
 
-    public static function encrypt($string) {
+    public static function encryptStr($string) {
         return base64_encode(openssl_encrypt($string, Security::$algoName, Security::$aesKey, Security::$algoType, Security::$ivKey));
     }
 
-    public static function decrypt($string) {
+    public static function decryptStr($string) {
         return openssl_decrypt(base64_decode($string), Security::$algoName, Security::$aesKey, Security::$algoType, Security::$ivKey);
+    }
+
+    public static function hashStr($string) {
+        return hash(Security::$algoHash, $string, false);
     }
 }
 ?>
